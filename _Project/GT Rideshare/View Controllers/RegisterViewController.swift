a6a1a02479d8a6a6a2cfdac526f3c321752d0588
jmschonfeld <jeremyschonfeld@gmail.com>
2019-01-25 14:04:51 -0500

    Process registration data

--- _Project/GT Rideshare/View Controllers/RegisterViewController.swift
+++ _Project/GT Rideshare/View Controllers/RegisterViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
@@ -15,6 +16,41 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func onRegister(_ sender: Any) {
+        guard let email = emailField.text, let password = passwordField.text, let name = nameField.text,
+            email != "", password != "", name != "" else {
+            let alert = UIAlertController(title: "Error", message: "Please fill out all required fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
         
+        guard email.hasSuffix("gatech.edu") else {
+            let alert = UIAlertController(title: "Error", message: "You must register with your gatech.edu email address", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            guard let _ = result else {
+                let alert = UIAlertController(title: "Error", message: "An unknown error ocurred. Please try again.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            
+            let alert = UIAlertController(title: "Success", message: "You have successfully created an account", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
     }
 }
