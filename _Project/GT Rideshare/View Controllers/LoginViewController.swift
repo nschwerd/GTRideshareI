b0d5039b591ef20e26b60cae98fd5361b759ad8d
jmschonfeld <jeremyschonfeld@gmail.com>
2019-01-25 13:57:30 -0500

    Process login info

--- _Project/GT Rideshare/View Controllers/LoginViewController.swift
+++ _Project/GT Rideshare/View Controllers/LoginViewController.swift
@@ -8,12 +8,39 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func onLogin(_ sender: Any) {
-        
+        guard let email = emailField.text, let password = passwordField.text, email != "", password != "" else {
+            let alert = UIAlertController(title: "Error", message: "Please fill out all required fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            guard let _ = result else {
+                let alert = UIAlertController(title: "Error", message: "An unknown error ocurred. Please try again.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            
+            let alert = UIAlertController(title: "Success", message: "You have successfully logged in", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
     }
 }
